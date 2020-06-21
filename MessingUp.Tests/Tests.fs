c693394b021fd0c145ed3d9b65a23789f26e2b35
Kerry Perret <kerry.perret@outlook.com>
2020-06-21 20:57:06 +0200

    test(dummy):  add dummy tests

--- MessingUp.Tests/Tests.fs
+++ MessingUp.Tests/Tests.fs
@@ -1,8 +1,37 @@
 module Tests
 
-open MessingUp
+open System
+open System.IO
+
+open Dapper
+
+open Npgsql
 
 open Xunit
+
+open Microsoft.Data.SqlClient
+open Microsoft.Extensions.Configuration
+
+open MessingUp
+
+
+type ConnectionStrings =
+    { SqlServer: string
+      PostgreSql: string }
+
+let connectionStrings =
+    let getEnvironmentName() = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    let getConnectionStrings() =
+        let configurationBuilder =
+            ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile(sprintf "appsettings.%s.json" (getEnvironmentName()), true, true)
+                .AddEnvironmentVariables()
+                .Build()
+        { SqlServer = configurationBuilder.GetConnectionString("sqlServer")
+          PostgreSql = configurationBuilder.GetConnectionString("postgreSql") }
+    lazy(getConnectionStrings())
 
 
 [<Fact>]
@@ -10,4 +39,20 @@ let ``This useless unit test should work!`` () =
     let name = "michelle"
     let actual = Say.hello name
     let expected = sprintf "Hello %s" name
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Connect to SQL Server``() =
+    use sqlServerConnection = new SqlConnection(connectionStrings.Value.SqlServer)
+    let query = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'"
+    let actual = sqlServerConnection.ExecuteScalar<int32>(query)
+    let expected = 5
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Connect to PostgreSQL``() =
+    use postgreSqlConnection = new NpgsqlConnection(connectionStrings.Value.PostgreSql)
+    let query = "SELECT COUNT(*) from information_schema.tables"
+    let actual = postgreSqlConnection.ExecuteScalar<int32>(query)
+    let expected = 194
     Assert.Equal(expected, actual)
